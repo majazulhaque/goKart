5e16eab44edd41e78296a5ba3dbdcff0b18945b9
Majaz <majazulhaque123@gmail.com>
2023-07-07 23:14:33 +0530

    Create Customer Schema

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const path = require("path");
 const port = 8000;
 const app = express();
 
+
+// Add config mongoose
 const db = require("./config/mongoose");
 
+
+app.use(express.json());
+
+// Add routes
+app.use("/", require("./routes"));
+
+app.use(express.urlencoded()); // middleware to analyse the req and convert into req.body.
 
 // Run the server on port 8000
 app.listen(port, function (err) {
