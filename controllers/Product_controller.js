cd59d3ea42d532dfc6c653bb73e4020273ba18bb
Majaz <majazulhaque123@gmail.com>
2023-07-07 23:30:37 +0530

    Implement Get Product and route it

--- controllers/Product_controller.js
+++ controllers/Product_controller.js
@@ -2,6 +2,48 @@ const Product = require("../models/Product");
 const Stock = require("../models/Stock");
 const Warehouse = require("../models/Warehouse");
 
+module.exports.getProducts = async function (req, res) {
+  try {
+    const products = await Product.find({});
+    const productList = [];
+    for (const product of products) {
+      const stock = await Stock.find({ product: product.name });
+      const stockQuantity = stock.reduce(
+        (total, item) => total + item.quantity,
+        0
+      );
+      const inStockWarehouses = await getInStockWarehouses(product.name);
+      // console.log(inStockWarehouses);
+      productList.push({
+        productName: product.name,
+        skuId: product.sku,
+        category: product.category,
+        subCategory: product.subCategory,
+        stockQuantity,
+        inStockWarehouses,
+      });
+    }
+    res.status(200).json(productList);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Helper function to get the list of in-stock warehouses for a product
+async function getInStockWarehouses(productName) {
+  const stocks = await Stock.find({ product: productName });
+  const inStockWarehouses = [];
+  for (const stock of stocks) {
+    const warehouses = await Warehouse.find({ warehouseName: stock.warehouse });
+    for (const warehouse of warehouses) {
+      inStockWarehouses.push({
+        warehouseNumber: warehouse.warehouseNumber,
+        warehouseName: warehouse.warehouseName,
+      });
+    }
+  }
+  return inStockWarehouses;
+}
 
 module.exports.add = async function (req, res) {
   try {
--- routes/Product.js
+++ routes/Product.js
@@ -5,8 +5,6 @@ const router = express.Router();
 const ProductController = require("../controllers/Product_controller");
 
 router.post("/add", ProductController.add);
-
-
-
+router.get("/", ProductController.getProducts);
 
 module.exports = router;
