9c1d745b96c47e33db3b4be32169637b2f99c3be
Majaz <majazulhaque123@gmail.com>
2023-07-08 00:19:03 +0530

    Implement log commands and add functionality for taking batch of input commands and store history of commands log

--- repl.js
+++ repl.js
@@ -8,24 +8,51 @@ async function startREPL() {
     output: process.stdout,
   });
 
+  const commandLogFilePath = "command-log.txt";
+  const commandLogStream = fs.createWriteStream(commandLogFilePath, { flags: "a" });
+  let commandBuffer = [];
+
   console.log("Welcome to the Product Inventory Management System.");
 
   rl.setPrompt("Enter a command: ");
   rl.prompt();
 
   rl.on("line", async (input) => {
-    const command = input.trim();
-    const result = await processCommand(command);
+    const commands = input.trim().split(";");
 
-    console.log(result);
+    for (const command of commands) {
+      // Add the command to the buffer
+      commandBuffer.push(command.trim());
+      const result = await processCommand(command.trim());
+      console.log(result);
+    }
+
+    // Check if the buffer has reached the batch size of 2 or more
+    if (commandBuffer.length >= 2) {
+      // Asynchronously stream the batch of commands to the command log file
+      await streamCommandBatch(commandBuffer);
+
+      // Remove the streamed commands from the buffer
+      commandBuffer.splice(0, 2);
+    }
 
     rl.prompt();
   });
 
-  rl.on("close", () => {
+  rl.on("close", async () => {
+    // Stream any remaining commands in the buffer to the command log file
+    if (commandBuffer.length > 0) {
+      await streamCommandBatch(commandBuffer);
+    }
+
     console.log("Exiting the Product Inventory Management System.");
     process.exit(0);
   });
+
+  async function streamCommandBatch(commands) {
+    const commandBatch = commands.slice(0, 2).join("\n");
+    commandLogStream.write(commandBatch + "\n");
+  }
 }
 
-module.exports = {startREPL};
+module.exports = { startREPL };
