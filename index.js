8a9fb3f06d5c27f193b31016bd2ff83f69c46559
Majaz <majazulhaque123@gmail.com>
2023-07-07 23:09:12 +0530

    Create and structure Folders and initialize server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const port = 8000;
+const app = express();
+
+
+
+
+// Run the server on port 8000
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error: ${err}`);
+  }
+  console.log(`Server is set up at port: ${port}`);
+});
+
+repl.startREPL();
